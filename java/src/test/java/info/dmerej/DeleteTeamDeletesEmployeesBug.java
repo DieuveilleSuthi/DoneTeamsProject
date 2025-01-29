3e01f0e675c5c4d184b400c20b6d31c485a89a6b
Léo TORRADO <leo.torrado@hotmail.fr>
2025-01-29 10:16:20 +0100

    DeleteTeam bug
    Automatiser le réplication du bug lorsqu'on delete une team, les membres de l'équipes sont aussi deleted

---
+++ java/src/test/java/info/dmerej/DeleteTeamDeletesEmployeesBug.java
@@ -0,0 +1,57 @@
+package info.dmerej;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+public class DeleteTeamDeletesEmployeesBug {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            var launchOptions = new BrowserType.LaunchOptions().setHeadless(false)
+                    .setSlowMo(1000); // Remove this when you're done debugging
+            Browser browser = playwright.chromium().launch(launchOptions);
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://d.se2.hr.dmerej.info/");
+
+            // Add a new team
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Create new team")).click();
+            page.getByPlaceholder("Name").fill("Bobteam");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
+
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Home")).click();
+
+
+            // Add a new employee
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee")).click();
+            page.getByPlaceholder("Name").fill("Bob");
+            page.getByPlaceholder("Email").fill("bob@gmail.com");
+            page.locator("#id_address_line1").fill("20 Bobstreet");
+            page.getByPlaceholder("City").fill("Bobville");
+            page.getByPlaceholder("Zip code").fill("1234");
+            page.getByPlaceholder("Hiring date").fill("2025-01-03");
+            page.getByPlaceholder("Job title").fill("Dev");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
+
+            // Add the new employee to the team
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Edit")).last().click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add to team")).click();
+            page.getByLabel("Team").selectOption("35"); // Incrémenter cette valeur !!
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Home")).click();
+
+            // Delete that team
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List teams")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Delete")).first().click();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed")).click();
+
+            // Check if the employee is deleted
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Home")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List employees")).click();
+
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Home")).click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List teams")).click();
+
+
+        }
+    }
+}
